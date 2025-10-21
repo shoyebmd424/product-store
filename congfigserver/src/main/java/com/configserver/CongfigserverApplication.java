a8419f7dbae3f3a0cea3ae574e1afc5e731e4325
Md Shoyeb <shoyebmd424>
2025-10-22 00:11:12 +0530

    configs

--- congfigserver/src/main/java/com/configserver/CongfigserverApplication.java
+++ congfigserver/src/main/java/com/configserver/CongfigserverApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class CongfigserverApplication {
 
 	public static void main(String[] args) {
